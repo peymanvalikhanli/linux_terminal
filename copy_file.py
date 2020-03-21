73c92c1d91aafaeeb60b173da830b0a6c6879e94
peyman valikhanli <peymanvalikhanli2012@gmail.com>
2020-03-21 21:23:22 +0430

    does not copy existing file

--- copy_file.py
+++ copy_file.py
@@ -6,10 +6,16 @@ import os
 from os import listdir
 from os.path import isfile, join
 
+
 dir_from = sys.argv[1]
 dir_to = sys.argv[2]
 
 onlyfiles = [f for f in listdir(dir_from) if isfile(join(dir_from, f))]
 
 for f in onlyfiles:
-    copyfile(dir_from+f, dir_to+f)
+    if not isfile(dir_to+f): 
+        # print('no copy ... ')
+        # print(f)
+    # else: 
+        copyfile(dir_from+f, dir_to+f)
+        # print(f)
